ed61329aec076b7a8c60bda7ab73f20e148b2181
Mtaaha94 <muhammadtaaha.94@gmail.com>
2021-07-03 18:29:31 +0500

    auth and signup login

--- src/App.js
+++ src/App.js
@@ -1,36 +1,91 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React, { Component } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { auth, handleUserProfile } from "./firebase/utils";
 //layouts
 import MainLayout from "./layouts/MainLayout";
 import HomepageLayout from "./layouts/HomepageLayout";
+//pages
 import Homepage from "./pages/Homepage";
 import Registration from "./pages/Registration";
+import Login from "./pages/Login";
 import "./default.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <Switch>
-        <Route
-          exact
-          path="/"
-          render={() => (
-            <HomepageLayout>
-              <Homepage />
-            </HomepageLayout>
-          )}
-        />
-        <Route
-          path="/registration"
-          render={() => (
-            <MainLayout>
-              <Registration />
-            </MainLayout>
-          )}
-        />
-      </Switch>
-    </div>
-  );
+const initialState = {
+  currenUser: null,
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ...initialState,
+    };
+  }
+
+  authListener = null;
+  componentDidMount() {
+    this.authListener = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await handleUserProfile(userAuth);
+        userRef.onSnapshot((snapshot) => {
+          this.setState({
+            currenUser: {
+              id: snapshot.id,
+              ...snapshot.data(),
+            },
+          });
+        });
+      }
+      this.setState({
+        ...initialState,
+      });
+    });
+  }
+  componentDidUnmount() {
+    this.authListener();
+  }
+  render() {
+    const { currenUser } = this.state;
+    return (
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <HomepageLayout currenUser={currenUser}>
+                <Homepage />
+              </HomepageLayout>
+            )}
+          />
+          <Route
+            path="/registration"
+            render={() =>
+              currenUser ? (
+                <Redirect to="/" />
+              ) : (
+                <MainLayout currenUser={currenUser}>
+                  <Registration />
+                </MainLayout>
+              )
+            }
+          />
+          <Route
+            path="/login"
+            render={() =>
+              currenUser ? (
+                <Redirect to="/" />
+              ) : (
+                <MainLayout currenUser={currenUser}>
+                  <Login />
+                </MainLayout>
+              )
+            }
+          />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDStkLFZxsBDdWZFDzY-BOXdeertQy8kvo",
+  authDomain: "ecommerce-website-5318c.firebaseapp.com",
+  projectId: "ecommerce-website-5318c",
+  storageBucket: "ecommerce-website-5318c.appspot.com",
+  messagingSenderId: "44958572960",
+  appId: "1:44958572960:web:51e1000c4bff56064c04c5",
+};
